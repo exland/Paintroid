acff6aace34e6b2f0b9e0ed247146445519f5dea
Tim Celec <timcelec1@gmail.com>
2023-09-26 12:52:55 +0200

    PAINTROID-663 SLIC  out of memory problems

--- Paintroid/src/main/java/org/catrobat/paintroid/tools/helper/PixelPixelAlgorithm.kt
+++ Paintroid/src/main/java/org/catrobat/paintroid/tools/helper/PixelPixelAlgorithm.kt
@@ -4,8 +4,11 @@ import android.R.attr
 import android.R.attr.bitmap
 import android.graphics.Bitmap
 import android.graphics.Color
+import androidx.annotation.VisibleForTesting
 import androidx.core.graphics.ColorUtils.RGBToLAB
 import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 
@@ -22,17 +25,28 @@ class PixelPixelAlgorithm(
             SuperPixel(x, y, doubleArrayOf(-10.0, -10.0, -10.0))
         }
     }
+    val pixelAssigments  =  Array(inputPix.width) { x ->
+        Array(inputPix.height) { y ->
+            DistanceCalculation(x, y)
+        }
+    }
+
     var inputImg =  inputPix
 
     var outputImg : Bitmap = inputImg.copy(inputImg.config, true)
     val iterations : Int =  130
+    @VisibleForTesting
     val N = inputImg.width * inputImg.height
+    @VisibleForTesting
     val K =  width * height
+    @VisibleForTesting
     val pixelsize : Double =  (N/K).toDouble()
+    @VisibleForTesting
     val superpixelInterval : Double  = sqrt(pixelsize)
+    @VisibleForTesting
     val superPixelheight =  inputImg.height / height
     val superPixelwidth =  inputImg.width / width
-
+    val m =  10 ;  // according to the paper
 
     fun getMean(input :Bitmap): Triple<Int, Int, Int> {
         var Red :Int = 0
@@ -79,12 +93,58 @@ class PixelPixelAlgorithm(
 
     }
 
-    fun assignPixelsSLIC()
-    {
-
+    fun calculatewSuperpixelcandidates(x :Int, y : Int): SupepixelCandidates {
+        val window =  (2 * superpixelInterval).roundToInt()
+        val xmin =  ((x - window).coerceAtLeast(0)/superPixelwidth.toDouble()).roundToInt()
+        var xmax =  ((x+ window).coerceAtMost(inputImg.width.toDouble().toInt() - 1 )/superPixelwidth.toDouble()).roundToInt()
+        if (xmax >= width){
+            xmax =  width - 1
+        }
+        val ymin =  ((y - window).coerceAtLeast(0)/superPixelheight.toDouble()).roundToInt()
+        var ymax = ((y + window).coerceAtMost(inputImg.height.toDouble().toInt() - 1 )/superPixelheight.toDouble()).roundToInt()
+        if(ymax >= height){
+            ymax = height - 1
+        }
+        return SupepixelCandidates(xmin, xmax, ymin, ymax)
     }
-    fun SLIC(bitmap : Bitmap)
+
+
+    fun distanceCalculation(xPixel: Int, yPixel : Int, lab : DoubleArray, superPixel :  SuperPixel ): Double {
+        val lDist = (superPixel.l - lab[0]).pow(2)
+        val aDist = (superPixel.a - lab[1]).pow(2)
+        val bDist = (superPixel.b -  lab[2]).pow(2)
+        val dLab= sqrt(lDist+aDist+bDist)
+        val xDist =  (superPixel.x.toDouble() - xPixel.toDouble()).pow(2)
+        val yDist =  (superPixel.y.toDouble() - yPixel.toDouble()).pow(2)
+        val xyDist =  (m * sqrt(xDist+ yDist))/ superpixelInterval
+        return dLab + xyDist
+    }
+
+    fun assignPixelsSLIC(input : Bitmap) {
+        for (x in 0 until input.width) {
+            for (y in 0 until input.height) {
+                val candidates = calculatewSuperpixelcandidates(x, y)
+                val c: Int = input.getPixel(x,y)
+                val Red = Color.red(c)
+                val Green = Color.green(c)
+                val Blue :Int = Color.blue(c)
+                val lab = doubleArrayOf(0.0,0.0,0.0)
+                RGBToLAB(Red,Green,Blue, lab)
+                for (XSup in candidates.xMin until candidates.xMax) {
+                    for (YSup in candidates.yMin until candidates.yMax) {
+                       val dist =  distanceCalculation(x,y, lab, superPixelArray[XSup][YSup])
+                        pixelAssigments[x][y].addSuperPixel(dist, Pair(XSup,YSup))
+
+                    }
+                }
+
+            }
+        }
+    }
+    fun SLIC()
     {
+        innitSLIC(inputImg)
+        assignPixelsSLIC(inputImg)
 
     }
 
--- Paintroid/src/main/java/org/catrobat/paintroid/tools/helper/SuperPixel.kt
+++ Paintroid/src/main/java/org/catrobat/paintroid/tools/helper/SuperPixel.kt
@@ -1,10 +1,36 @@
 package org.catrobat.paintroid.tools.helper
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 
 class SuperPixel(arg_x: Int, arg_y: Int, lab : DoubleArray)  {
     var x: Int = arg_x
     var y: Int = arg_y
-    var l: Double = lab.get(0)
-    var a: Double = lab.get(1)
-    var b: Double = lab.get(2)
+    var l: Double = lab[0]
+    var a: Double = lab[1]
+    var b: Double = lab[2]
+}
+
+data class SupepixelCandidates(val xMin : Int, val xMax : Int, val yMin : Int, val yMax : Int)
+
+class DistanceCalculation(argX: Int, argY :Int) {
+    private val x: Int = argX
+    private val y: Int = argY
+    private val doubleToPairMap = LinkedHashMap<Double, Pair<Int, Int>>()
+    var smallestKey: Double = Double.MAX_VALUE
+
+    fun addSuperPixel(distance : Double, sup_pix : Pair<Int, Int> ){
+        doubleToPairMap[distance] = sup_pix
+    }
+    fun getShortestDistance(): Double {
+
+        for (entry in doubleToPairMap) {
+            if (entry.key < smallestKey) {
+                smallestKey = entry.key
+            }
+        }
+        return smallestKey
+    }
+
 }
